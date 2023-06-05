ae9db043645d73903d4a3925150e621c3ad4891f
José Antônio <antoniojaraujo0@gmail.com>
2023-06-05 20:32:45 -0300

    working/formating the date

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,49 @@ import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 import styles from "./App.module.css";
 
+// author: {avatar_url: string, name: string, role: string}
+//publishedAt: Date
+//content: string
+
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: "https://github.com/allanhal.png",
+			name: "José Antonio",
+			role: "Web Developer",
+		},
+		content: [
+			{ type: "paragraph", content: "Fala galera 👍" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir o meu site. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+			},
+			{ type: "link", content: "jane.design/dkasjkasjdksl" },
+		],
+		publishedAt: new Date("2023-06-05 12:30:30"),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: "https://github.com/joseaantonio.png",
+			name: "Allan Araujo",
+			role: "Web Developer",
+		},
+		content: [
+			{ type: "paragraph", content: "Fala galera 👍" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir o meu site. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+			},
+			{ type: "link", content: "jane.design/dkasjkasjdksl" },
+		],
+		publishedAt: new Date("2023-06-05 12:30:30"),
+	},
+];
+
 export function App() {
 	return (
 		<>
@@ -12,10 +55,16 @@ export function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post
-						author='John Doe'
-						content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat provident accusamus sit amet, magni laborum dignissimos, facere dolorem soluta optio ducimus cumque iste esse hic repellendus voluptatum veritatis incidunt iusto!'
-					/>
+					{posts.map((post) => {
+						return (
+							<Post
+								key={post.id}
+								author={post.author}
+								content={post.content}
+								publishedAt={post.publishedAt}
+							/>
+						);
+					})}
 				</main>
 			</div>
 		</>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,41 +1,52 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+
 import styles from "./Post.module.css";
+
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
 
-export const Post = () => {
+export const Post = ({ author, publishedAt, content }) => {
+	const formatedDate = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+		locale: ptBR,
+	});
+
+	const dateRelativeToNow = formatDistanceToNow(publishedAt, {
+		locale: ptBR,
+		addSuffix: true,
+	});
+
 	return (
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
-					<Avatar src='https://github.com/allanhal.png' alt='avatar' />
+					<Avatar src={author.avatarUrl} alt='avatar' />
 					<div className={styles.authorInfo}>
-						<strong>José Antonio</strong>
-						<span>Web Developer</span>
+						<strong>{author.name}</strong>
+						<span>{author.role}</span>
 					</div>
 				</div>
 
-				<time
-					dateTime='2023-06-05 12:30:30'
-					title='Publicado em 5 de junho de 2023'
-				>
-					Publicado há 1 hora
+				<time dateTime={publishedAt.toISOString()} title={formatedDate}>
+					{dateRelativeToNow}
 				</time>
 			</header>
 
 			<div className={styles.content}>
-				<p>Fala galera 👍</p>
-				<p>
-					Acabei de subir o meu site. É um projeto que fiz no NLW Return, evento
-					da Rocketseat. O nome do projeto é DoctorCare 🚀
-				</p>
-				<p>
-					<a href='#'>jane.design/dkasjkasjdksl</a>
-				</p>
-				<p>
-					<a href='#'>#novoprojetocketo</a> <a href='#'>#rocketseat</a>{" "}
-					<a href='#'>#nlw</a>
-				</p>
+				{content.map((line) => {
+					if (line.type === "paragraph") {
+						return <p>{line.content}</p>;
+					} else if (line.type === "link") {
+						return (
+							<p>
+								<a href='#'>{line.content}</a>
+							</p>
+						);
+					}
+				})}
 			</div>
 
 			<form className={styles.commentForm}>
